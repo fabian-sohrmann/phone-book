9f256a9301d50a72a140310c1772c8f2c94a1027
Fabian Sohrmann <mfsohr@utu.fi>
2022-03-13 19:23:50 +0200

    2.6 valmis

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ class App extends React.Component {
     super(props)
     this.state = {
       persons: [
-        { name: 'Arto Hellas'}
+        { name: 'Arto Hellas', number: '040-123456'}
       ],
-      newName: ''
+      newName: '',
+      newNumber: ''
     }
   }
 
@@ -17,11 +18,12 @@ class App extends React.Component {
     event.preventDefault()
     const result = this.state.persons.find(elem => elem.name === this.state.newName)
     if(result===undefined){
-      const personObject = {name: this.state.newName}
+      const personObject = {name: this.state.newName, number: this.state.newNumber}
       const persons = this.state.persons.concat(personObject)
       this.setState({
         persons: persons,
-        newName: '' 
+        newName: '', 
+        newNumber: ''
       })
     }else{
       alert("Cannot have the same name twice!")
@@ -29,8 +31,11 @@ class App extends React.Component {
   }
 
   handlePersonChange = (event) => {
-    console.log(event.target.value)
     this.setState({ newName: event.target.value })
+  }
+
+  handleNumberChange = (event) => {
+    this.setState({ newNumber: event.target.value })
   }
 
   render() {
@@ -39,23 +44,21 @@ class App extends React.Component {
         <h2>Puhelinluettelo</h2>
         <form onSubmit={this.addPerson}>
           <div>
-            nimi: <input 
-                    value={this.state.newName} 
-                    onChange={this.handlePersonChange}
-                  />
+            nimi: <input value={this.state.newName} onChange={this.handlePersonChange}/>
+          </div>
+          <div>
+            numero: <input value={this.state.newNumber} onChange={this.handleNumberChange}/>
           </div>
           <div>
             <button type="submit">lisää</button>
           </div>
         </form>
         <h2>Numerot</h2>
-        {this.state.persons.map(person => <p key={person.name}>{person.name}</p>)}
-        
-        {/*
-        MIKSI EI TOIMI SEURAAVALLA KOODIRIVILLÄ JA ERILLISELLÄ PERSON-KOMPONENTILLA? 
-        <Person key={person.id} name={person.name} /> 
-        TULEE VIRHE: TARVITAAN UNIQUE KEY */} 
-        
+        <table>
+          <tbody>
+            {this.state.persons.map(person => <tr key={person.name}><td>{person.name}</td><td>{person.number}</td></tr>)}
+          </tbody>
+        </table>
       </div>
     )
   }
